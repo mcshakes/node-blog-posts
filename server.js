dfb40b78c855a47764811fab3249e418a2b6e167
Edward McCarthy <efmccarthy26@gmail.com>
2018-04-11 12:28:47 -0600

    Finished the Modelling for blogposts

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
-
+const BlogPosts = require("./models")
 // GET and POST requests should go to /blog-posts
+
+BlogPosts.create("How to smoke Kielbasa", "something something something", "Daud McSheigh", );
+
 app.get("/blog-posts", (req,res) => {
   res.json(BlogPosts.get());
 })
