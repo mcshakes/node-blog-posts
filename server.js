d614993c95082ef938007e2ec242d95638fdc95c
Edward McCarthy <efmccarthy26@gmail.com>
2018-04-11 12:48:10 -0600

    Have the CREATE working now

--- server.js
+++ server.js
@@ -8,10 +8,29 @@ const jsonParser = bodyParser.json();
 const app = express();
 app.use(morgan('common'));
 
-BlogPosts.create("How to smoke Kielbasa", "something something something", "Daud McSheigh");
+BlogPosts.create("How to smoke Kielbasa", "All in the rub, baby. For the heat level is the end game", "Daud McSheigh");
+BlogPosts.create("Capitalism Reimagined", "Greed is good", "Shania Twilip");
+BlogPosts.create("Stop Reading", "Books are evil: change my mind", "Gilbert Melendez");
 
 app.get("/blog-posts", (req,res) => {
   res.json(BlogPosts.get());
+})
+
+app.post("/blog-posts", jsonParser, (req, res) => {
+  const requiredFields = ["title", "content", "author"];
+
+  for (let i = 0; i <requiredFields.length; i++) {
+    const field = requiredFields[i];
+
+    if (!(field in req.body)) {
+      let message = `Missing \`${field}\` in request body`
+      console.error(mesage);
+      return res.status(400).send(message);
+    }
+  }
+
+  const blogPost = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+  res.status(201).json(blogPost);
 })
 
 
