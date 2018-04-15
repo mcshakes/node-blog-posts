b6802aa4eee19818b00ae59ee64035c9d72e9449
Edward McCarthy <efmccarthy26@gmail.com>
2018-04-14 22:16:10 -0600

    Broke server up into a Run and a Close functions for the tests

--- server.js
+++ server.js
@@ -10,11 +10,44 @@ const app = express();
 
 app.use(morgan('common'));
 
-
 const blogPostsRouter = require("./blogPostsRouter")
 
 app.use("/blog-posts", blogPostsRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`App is listening on port ${process.env.PORT || 8080}`)
-} )
+
+let server; // global server so it's the same server on 24 and 36
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is running on port ${port}`);
+      resolve(server);
+    }).on("error", err => {
+      reject(err)
+    });
+  })
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server");
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call resolve
+        return;
+      }
+      resolve();
+    })
+  })
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.log(err));
+}
+
+// app.listen(process.env.PORT || 8080, () => {
+//   console.log(`App is listening on port ${process.env.PORT || 8080}`)
+// } )
